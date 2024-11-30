a69fc59b0541081ff58deebd68cb8f47d9f6b1cc
Cletous <ngomacletousjnr@gmail.com>
2024-11-30 22:09:53 +0200

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import logo from '../images/branding/black-logo.png'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <Link to="main" className="logo">
+                    <img src={logo} alt="" />
+                </Link>
+
+                <ul className="main-menu">
+                    <li><Link to="main" className="active">Home</Link></li>
+                    <li><Link to="about" className="active">About Us</Link></li>
+                    <li><Link to="contact" className="active">Contact Us</Link></li>
+                    <li><Link to="auth" className="active">Register/Login</Link></li>
+                </ul>
+                <Link to="#" className="button">Login</Link>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
